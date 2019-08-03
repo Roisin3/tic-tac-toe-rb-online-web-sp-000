086c649b7be98326e1f4ee7384adcd8fa007a064
Michael Lynch <michael@michaelrlynch.com>
2019-08-03 18:26:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -93,10 +93,9 @@ end
 
 def play(board)
   turn(board) until over?(board)
-    if won?(board)
-      puts "Congratulations #{winner(board)}!"
-    elsif draw?(board)
-      puts "Cat's Game!"
-    end
-
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
 end
