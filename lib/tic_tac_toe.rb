d510f8fd17ce14f85ff42b41c5da31be3dac5582
Michael Lynch <michael@michaelrlynch.com>
2019-08-03 18:30:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,7 +42,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-    move(board, index, choice = "X")
+    move(board, index, choice)
     display_board(board)
   else
     turn(board)
