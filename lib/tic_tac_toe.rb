8c45c5415c2797785f94dd95d12e886fd7a20c4c
Michael Lynch <michael@michaelrlynch.com>
2019-08-03 13:58:50 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,92 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+WIN_COMBINATIONS = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+]
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, choice = "X")
+  board[index] = choice
+end
+
+def position_valid?(board, index)
+  board[index] != " "
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8)
+    if !position_valid?(board, index)
+      true
+    end
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, choice = "X")
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  turn_number = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      turn_number += 1
+    end
+  end
+  turn_number
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |w_index|
+    board[w_index[0]] == board[w_index[1]] &&
+    board[w_index[1]] == board[w_index[2]] &&
+    position_taken?(board, w_index[0])
+  end
+end
+
+def full?(board)
+  board.all? do |blank|
+    blank == "X" || blank == "O"
+  end
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board) || draw?(board)
+end
+
+def winner(board)
+  if win = won?(board)
+    board[win.first]
+  end
+end
